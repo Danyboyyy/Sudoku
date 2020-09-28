fe54eb82937f04e818e5d2f09154bf467b3ae782
Daniel David <danielalejandro.2000@outlook.com>
2020-09-28 15:31:52 -0500

    Sudoku solving algorithm finished

--- sudoku/src/solution/sudoku-solution.js
+++ sudoku/src/solution/sudoku-solution.js
@@ -1,6 +1,22 @@
 export let sudokuSolution = (b) => {
-  if (solveSudoku(b))
-    return b;
+  if (checkInput(b)) {
+    if (solveSudoku(b))
+      return b
+  }
+  else {
+    console.log('unsolvable');
+    return [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+  }
 }
 
 function isValid(board, row, col, val) {
@@ -11,6 +27,33 @@ function isValid(board, row, col, val) {
       return false;
   }
   return true;
+}
+
+function isValid2(board, row, col, val) {
+  for (let i = 0; i < 9; i++) {
+    const rowBox = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const colBox = 3 * Math.floor(col / 3) + i % 3;
+    if (board[row][i] === val || board[i][col] === val || board[rowBox][colBox] === val) {
+      if ((i !== col && i !== row) && (rowBox !== row || colBox !== col)) {
+        return false; 
+      }
+    }
+  }
+  return true;
+}
+
+function checkInput(_board) {
+  let aux = true;
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (_board[i][j] !== 0) {
+        if (!isValid2(_board, i, j, _board[i][j])) {
+          return false;
+        }
+      }
+    }
+  }
+  return aux;
 }
 
 function solveSudoku(board) {
